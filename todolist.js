0087659ce2eca46ce0fd88482dd66b03a81037b1
chagaleti asha <ashaharish128@gmail.com>
2024-12-10 21:47:49 +0530

    deploy

---
+++ todolist.js
@@ -0,0 +1,45 @@
+function addTask(){
+ var taskInput= document.getElementById("taskinput").value
+ var tasklist=document.getElementById("tasklist")
+ if(taskInput=="")
+ {
+    alert("please enter the task");
+    return;
+ }
+ var listItems=document.createElement('li');
+  listItems.innerText=taskInput;
+  tasklist.appendChild(listItems);
+
+
+  var buttoncontainer=document.createElement('div');
+  listItems.appendChild(buttoncontainer);
+  buttoncontainer.className="task-button";
+
+  var deletebutton=document.createElement('button');
+  buttoncontainer.appendChild(deletebutton)
+  deletebutton.innerText="Delete";
+  deletebutton.onclick=function(){
+    tasklist.removeChild(listItems);
+
+    clearInput();
+  };
+  buttoncontainer.appendChild(deletebutton);
+
+  
+
+  var completebutton=document.createElement('button');
+  buttoncontainer.appendChild(completebutton);
+  completebutton.innerText="Complete";
+  completebutton.onclick=function(){
+  listItems.classList.toggle('complete');
+    clearInput();
+  };
+  buttoncontainer.appendChild(completebutton);
+   clearInput();
+}
+function clearInput(){
+  document.getElementById("taskinput").value ="";
+}
+  
+
+
